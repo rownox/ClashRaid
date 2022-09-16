45f682fae0e9fae9d4c1558e6ed3def60d29b99b
rownox <rownox.ik@gmail.com>
2022-09-15 22:47:43 -0400

    leave event

--- src/main/java/me/rownox/clashraid/Events/LeaveEvent.java
+++ src/main/java/me/rownox/clashraid/Events/LeaveEvent.java
@@ -2,11 +2,13 @@ package me.rownox.clashraid.Events;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class LeaveEvent implements Listener {
 
+    @EventHandler
     public void onLeave(PlayerQuitEvent e) {
         Player p = e.getPlayer();
         String name = p.getName();
