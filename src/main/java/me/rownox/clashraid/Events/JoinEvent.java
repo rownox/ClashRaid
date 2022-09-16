48f1f0c3993ccc6ac996f770dceeb59fe710b971
yes <fryrbeadcider@gmail.com>
2022-09-15 23:03:34 -0400

    tarew

---
+++ src/main/java/me/rownox/clashraid/Events/JoinEvent.java
@@ -0,0 +1,25 @@
+package me.rownox.clashraid.Events;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class JoinEvent implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        Player p = e.getPlayer();
+        String name = p.getName();
+        Location loc = new Location(Bukkit.getWorld("spawn"), 0, 60, 0, 0, 0);
+
+        p.teleport(loc);
+        Bukkit.broadcastMessage(name + " Joined the game.");
+
+    }
+
+
+}
